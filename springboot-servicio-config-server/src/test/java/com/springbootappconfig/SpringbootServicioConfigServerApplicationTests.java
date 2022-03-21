b1bdeccfe66f150e27c4425baaf777795d6cbe71
Eli Olivera <82001173+ElizabethOlivera111174@users.noreply.github.com>
2022-03-21 16:32:02 -0300

    Add files via upload

---
+++ springboot-servicio-config-server/src/test/java/com/springbootappconfig/SpringbootServicioConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootappconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootServicioConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
